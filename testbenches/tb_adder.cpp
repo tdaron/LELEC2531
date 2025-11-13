0a9a55e8ced2244661bcc8549c9a48e509e91c3d
Théo Daron <theo@daron.be>
2025-11-13 17:46:16 +0100

    adder

--- testbenches/tb_adder.cpp
+++ testbenches/tb_adder.cpp
@@ -6,10 +6,25 @@
 
 int main(int argc, char **argv, char **env) {
   Vadder *dut = new Vadder;
+
   dut->a = 6;
   dut->b = 7;
+  dut->modulant = 9;
   dut->eval();
-  assert(dut->out == 13);
+  assert(dut->out == 4);
+
+  dut->a = 6;
+  dut->b = 2;
+  dut->modulant = 9;
+  dut->eval();
+  assert(dut->out == 8);
+
+  dut->a = 6;
+  dut->b = 3;
+  dut->modulant = 9;
+  dut->eval();
+  assert(dut->out == 0);
+
   std::cout << "done \n";
   delete dut;
   exit(EXIT_SUCCESS);
