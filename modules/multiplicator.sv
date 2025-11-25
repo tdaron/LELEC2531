e8eba6a9fe69d2b161797f712a60a880787dcadf
Your mum <yourmum@brothel.com>
2025-11-25 18:26:27 +0100

    multiplication done

--- modules/multiplicator.sv
+++ modules/multiplicator.sv
@@ -6,14 +6,42 @@ module multiplicator #(parameter DATA_WIDTH = 8)(
   input logic [DATA_WIDTH-1:0] a,
   input logic [DATA_WIDTH-1:0] b,
   input logic [DATA_WIDTH-1:0] modulant,
+  input logic [DATA_WIDTH-1:0] R_div_two,
+  input logic [DATA_WIDTH-1:0] R_squared,
   output logic [DATA_WIDTH-1:0] out
 );
 
-logic [2*DATA_WIDTH-1:0] intermidiate;
-    
-assign intermidiate = a * b;
+logic [DATA_WIDTH-1:0] ma;
+logic [DATA_WIDTH-1:0] mb;
+logic [DATA_WIDTH-1:0] mc;
 
-assign out = intermidiate % modulant;
+montgomery_reduce #(.DATA_WIDTH(DATA_WIDTH)) r1(
+  .t(a * R_squared),
+  .modulant(modulant),
+  .R_div_two(R_div_two),
+  .out(ma)
+);
+
+montgomery_reduce #(.DATA_WIDTH(DATA_WIDTH)) r2(
+  .t(b * R_squared),
+  .modulant(modulant),
+  .R_div_two(R_div_two),
+  .out(mb)
+);
+
+montgomery_reduce #(.DATA_WIDTH(DATA_WIDTH)) rc(
+  .t(ma * mb),
+  .modulant(modulant),
+  .R_div_two(R_div_two),
+  .out(mc)
+);
+
+montgomery_reduce #(.DATA_WIDTH(DATA_WIDTH)) r_final(
+  .t(mc),
+  .modulant(modulant),
+  .R_div_two(R_div_two),
+  .out(out)
+);
 
 endmodule;
 
--- testbenches/tb_multiplicator.cpp
+++ testbenches/tb_multiplicator.cpp
@@ -7,23 +7,15 @@
 int main(int argc, char **argv, char **env) {
   Vmultiplicator *dut = new Vmultiplicator;
 
-  dut->a = 6;
-  dut->b = 7;
-  dut->modulant = 9;
+  dut->a = 185;
+  dut->b = 209;
+  dut->modulant = 213;
+  dut->R_div_two = 128;
+  dut->R_squared = 145;
+ 
   dut->eval();
-  assert(dut->out == 6);
-
-  dut->a = 1;
-  dut->b = 7;
-  dut->modulant = 9;
-  dut->eval();
-  assert(dut->out == 7);
-
-  dut->a = 6;
-  dut->b = 3;
-  dut->modulant = 9;
-  dut->eval();
-  assert(dut->out == 0);
+  
+  printf("%d\n", dut->out);
 
   std::cout << "Multiplicator Test: done \n";
   delete dut;
